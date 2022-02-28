90156e566d0ede1c6f17d1edde34cec9082b1f36
AishwaryaBiswal1 <97431951+AishwaryaBiswal1@users.noreply.github.com>
2022-02-28 17:48:48 +0530

    Add files via upload

---
+++ TestNG/Activities/DemoOne.java
@@ -0,0 +1,13 @@
+package suiteExample;
+
+import org.testng.annotations.Test;
+public class DemoOne {
+    @Test
+    public void firstTestCase() {
+        System.out.println("First test case ");
+    }
+    @Test
+    public void secondTestCase() {
+        System.out.println("ISecond test case");
+    }
+}
